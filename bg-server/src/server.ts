534fb09cf43daa261cc762ad6f3806fa4c4f1b01
Debargha Mondal <debarghamondal227@gmail.com>
2025-03-11 20:07:42 +0530

    Added server

---
+++ bg-server/src/server.ts
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import connectDB from "./configs/mongodb";
+
+//App Config
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+
+await connectDB();
+
+// Initialize Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+// API routes
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "API is Working" });
+});
+
+// Start Server
+app.listen(PORT, () => console.log("🚀 Server Running on port " + PORT));
